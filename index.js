346aabef14700434291fbdeba7354c3642006fe0
Jennifer Browning <browningjen@yahoo.com>
2018-11-06 18:30:06 -0500

    first attempt

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 const { Client } = require ('pg');
-const express = require ('express');
-const port = process.env.PORT || 3000;
+const express = require("express");
 
 const app = express();
 app.use(express.json());
-
-const client = new Client({
+const client = new Client ({
     database: 'social-media'
 });
 
-client.connect();
 
-//route handlers go here
 app.get('/users', (req, res) => {
     client.query('SELECT * FROM users', (err, result) => {
+        if (err){
+            console.log(err);
+            res.status(500);
+            return res.send()
+        }
         res.send(result.rows);
     });
-    console.log()
 });
 
 app.post('/users', (req, res) => {
+    
     const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
-    const values = ['kenzie', 'Kenzie Adademy'];
+    const values = [req.body.username, req.body.bio]
+    
     client.query(text, values, (err, result) => {
         console.log(result.rows[0]);
-})
+        res.status(201);
+        res.send(result.rows[0])
+    });
 });
 
-app.listen(port, () => {
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id
+    const values = [id]
+    client.query('SELECT * FROM users WHERE id=$1', values, (err, result) => {
+        if (err){
+            console.log(err);
+            res.status(500);
+            return res.send()
+        }
+        if(result.rows.length === 0){
+            res.status(404);
+            return res.send()
+
+        }
+        res.send(result.rows[0]);
+    });
+});
+
+
+app.listen(3000, () => {
     client.connect();
 });
--- queries/01_create_users_table.sql
+++ queries/01_create_users_table.sql
@@ -1,5 +1,6 @@
  CREATE TABLE IF NOT EXISTS users (
         id serial PRIMARY KEY,
         username varchar(15),
-        bio varchar(255),
+        bio varchar(255)
     );
+
